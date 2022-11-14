da2e10dd6696e2230198dd31e3107818b0b1ecc6
angel <dehainaut.angelique@orange.fr>
2022-11-14 16:47:09 +0100

    cours webpack live

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,25 @@
 const webpack = require('webpack');
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const {loader} = require("mini-css-extract-plugin");
-const CopyPlugin = require("copy-webpack-plugin"); // Module Node utilisable avec Webpack, sert à résoudre les chemins relatifs
+const development = require("./webpack.config.dev");
+const production = require("./webpack.conf.prod");
 
-module.exports = {
-    // Point d'entrée Javascript, fichier qui contiendra vos includes
-    entry: {
-        front: './assets/js/front/main.js',
-        admin: './assets/js/admin/main.js'
-    },
-    //Objet contenant le chemin de sortie, ainsi que le nom à donner au fichier
-    output: {
-        path: path.resolve(__dirname, 'public/build'),
-        filename: "./js/[name]-bundle.js",
-        publicPath: "",
-    },
+module.exports  = (env, argv) => {
+    const configuration = argv.mode === 'development' ? development : production;
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            import: true,
-                            url : false,
-                            sourceMap: true
-                        }
-                    },
-
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: "[name] . [ext]",
-                        outputPath : 'images/',
-                        publicPath : 'build/images/'
-                    }
-                }
-                ]
-            }
-        ],
-    },
-
-    optimization: {
-        minimizer: [
-            new CssMinimizerPlugin(),
-        ]
-    },
-
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: './css/[name].css',
-        }),
-
-        new CopyPlugin({
-            patterns: [
-                {from: 'assets/styles/images/' , to: 'images/'}
-            ]
-        }),
-    ]
+    return {
+        // Point d'entrée Javascript, fichier qui contiendra vos includes
+        entry: {
+            front: './assets/js/front/main.js',
+            admin: './assets/js/admin/main.js'
+        },
+        //Objet contenant le chemin de sortie, ainsi que le nom à donner au fichier
+        output: {
+            path: path.resolve(__dirname, 'public/build'),
+            filename: "./js/[name]-bundle.js",
+            publicPath: "/build/",
+            clean: true,
+        },
+        ...configuration,
+    }
 };
+
